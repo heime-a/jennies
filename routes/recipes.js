f14a8134ad38372d960150271e81a0e6c9e3d57e
Heime Ashkenazue <heime.a@gmail.com>
2018-11-20 12:13:54 -0500

    Added recipe crud routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,7 @@
 'use strict;';
 
 const express = require('express');
-// const Ingredient = require('../models/ingredient');
+const Ingredient = require('../models/ingredient');
 const Recipe = require('../models/recipe');
 
 const router = express.Router();
@@ -13,6 +13,78 @@ router.get('/', async (req, res) => {
     message: 'All Recipes',
     content: allrecipes,
   });
+});
+
+router.post('/', async (req, res) => {
+  const { name, manHours, ingredients } = req.body;
+  const recipe = new Recipe();
+  recipe.name = name;
+  recipe.manHours = manHours;
+  recipe.ingredients = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const i of ingredients) {
+    // eslint-disable-next-line no-await-in-loop
+    const ingredient = await Ingredient.findOne({ name: i.ingredient.name });
+    if (ingredient) {
+      recipe.ingredients.push({ ingredient, quantity: i.quantity });
+    } else {
+      res.json({
+        message: 'ingredient not found',
+        data: req.body,
+      });
+    }
+  }
+
+  try {
+    await recipe.save();
+    res.json({
+      message: 'Saved new record',
+      data: recipe,
+    });
+  } catch (error) {
+    res.json({
+      message: 'Record not saved',
+      data: recipe,
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { name, manHours, ingredients } = req.body;
+  console.log(`Saving: ${req.params.id} ${name} ${manHours} ${ingredients}`);
+  const recipe = {};
+  recipe.name = name;
+  recipe.manHours = manHours;
+  recipe.ingredients = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const i of ingredients) {
+    // eslint-disable-next-line no-await-in-loop
+    const ingredient = await Ingredient.findOne({ name: i.ingredient.name });
+    if (ingredient) {
+      recipe.ingredients.push({
+        ingredient,
+        quantity: i.quantity,
+      });
+    } else {
+      res.json({
+        message: 'ingredient not found',
+        data: JSON.stringify(req.body),
+      });
+    }
+  }
+  try {
+    const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, recipe);
+    res.json({
+      message: 'Updated new record',
+      data: JSON.stringify(updatedRecipe),
+    });
+  } catch (err) {
+    res.json({
+      message: `Record not updated : ${err.stack}`,
+      data: recipe,
+    });
+  }
 });
 
 module.exports = router;
