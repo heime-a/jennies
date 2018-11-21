06b879e7817440d036a0bd469f6a21899f04dba6
Heime Ashkenazue <heime.a@gmail.com>
2018-11-21 16:52:18 -0500

    WorkorderList first cut

---
+++ src/components/WorkOrderList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WorkOrderList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  content : [
+            
+        ]};
+    }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default WorkOrderList;
