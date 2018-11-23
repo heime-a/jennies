2cef142a698076c14d37bb0bff84bd9a25fa766d
Heime Ashkenazue <heime.a@gmail.com>
2018-11-23 14:56:44 -0500

    WIP work orders

--- src/components/WorkOrderList.js
+++ src/components/WorkOrderList.js
@@ -4,6 +4,13 @@ class WorkOrderList extends Component {
     constructor(props) {
         super(props);
         this.state = {  content : [
+            {
+                woNumber: 'wo0001',
+                recipeName: 'Chocolate Macaroons',
+                startDate: '01/01/80',
+                actualHours: 5
+            },
+
             
         ]};
     }
