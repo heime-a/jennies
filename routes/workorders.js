54bbf168fd6bbf0ae1d5ded6a18adc3c8512679a
Heime Ashkenazue <heime.a@gmail.com>
2018-11-29 12:01:34 -0500

    workorder route first commit

---
+++ routes/workorders.js
@@ -0,0 +1,74 @@
+'use strict;';
+
+// @ts-check
+
+const express = require('express');
+const WorkOrder = require('../models/workorder');
+const Recipe = require('../models/recipe');
+
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  const allworkorders = await WorkOrder.find({}).populate('recipe');
+
+  res.json({
+    message: 'All WorkOrders',
+    content: allworkorders,
+  });
+});
+
+router.post('/', async (req, res) => {
+  const {
+    recipeName, startDate, status, actualHours,
+  } = req.body;
+
+  const wo = new WorkOrder();
+
+  wo.recipe = await Recipe.findOne({ name: recipeName });
+
+  wo.startDate = startDate;
+  wo.status = status;
+  wo.actualHours = actualHours;
+  try {
+    const savedWo = await wo.save();
+    res.json({
+      message: 'WorkOrder Saved',
+      content: savedWo,
+    });
+  } catch (error) {
+    res.json({
+      message: 'Workorder not saved',
+      content: error,
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const {
+    recipeName, startDate, status, actualHours,
+  } = req.body;
+
+  const recipe = Recipe.findOne({ name: recipeName });
+
+  const wo = {};
+
+  wo.recipe = recipe;
+  wo.startDate = startDate;
+  wo.status = status;
+  wo.actualHours = actualHours;
+  try {
+    const updatedWO = await WorkOrder.findByIdAndUpdate(req.params.id, wo);
+    res.json({
+      message: 'Updated work order',
+      content: updatedWO,
+    });
+  } catch (error) {
+    res.json({
+      message: 'Update failed',
+      content: error,
+    });
+  }
+});
+
+module.exports = router;
