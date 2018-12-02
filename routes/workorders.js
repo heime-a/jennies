999c99945c884dfa039a69fcfa6d38d798966935
Heime Ash <heime@outlook.com>
2018-12-02 09:52:15 -0500

    first cut at validate work order

--- routes/workorders.js
+++ routes/workorders.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+
 'use strict;';
 
 // @ts-check
@@ -5,8 +7,44 @@
 const express = require('express');
 const WorkOrder = require('../models/workorder');
 const Recipe = require('../models/recipe');
+const PurchaseOrder = require('../models/purchaseorders');
 
 const router = express.Router();
+
+function validateWorkOrder(recipeIngredients, inventory) {
+  for (const i of recipeIngredients) {
+    const itm = inventory.find(el => el.name === i.ingredient.name);
+    if (i.quantity > itm.quantity) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function getInventory() {
+  const response = await PurchaseOrder.aggregate([
+    {
+      $unwind: {
+        path: '$ingredients',
+      },
+    }, {
+      $lookup: {
+        from: 'ingredients',
+        localField: 'ingredients.ingredient',
+        foreignField: '_id',
+        as: 'ingredients.ingredient',
+      },
+    }, {
+      $group: {
+        _id: '$ingredients.ingredient.name',
+        total: {
+          $sum: '$ingredients.quantity',
+        },
+      },
+    },
+  ]);
+  return response.map(({ _id, total }) => ({ name: _id[0], quantity: total }));
+}
 
 
 router.get('/', async (req, res) => {
@@ -20,47 +58,55 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const {
-    recipe, startDate, status, actualHours,
+    woNumber, recipe, startDate, status, actualHours,
   } = req.body;
 
   const wo = new WorkOrder();
 
-  wo.recipe = await Recipe.findOne({ name: recipe.name });
+  wo.recipe = await Recipe.findOne({ name: recipe.name }).populate('ingredients.ingredient');
 
   wo.startDate = startDate;
   wo.status = status;
   wo.actualHours = actualHours;
-  try {
-    const savedWo = await wo.save();
-    res.json({
-      message: 'WorkOrder Saved',
-      content: savedWo,
-    });
-  } catch (error) {
-    res.json({
-      message: 'Workorder not saved',
-      content: error,
-    });
+  wo.woNumber = woNumber;
+
+  const inventory = await getInventory();
+
+  const canManufacture = validateWorkOrder(wo.recipe.ingredients, inventory);
+
+  if (!canManufacture) {
+    res.json({ message: ' Cant Make this not enough inventory', content: [wo.recipe, inventory] });
+  } else {
+    try {
+      const savedWo = await wo.save();
+      res.json({
+        message: 'WorkOrder Saved',
+        content: savedWo,
+      });
+    } catch (error) {
+      res.json({
+        message: 'Workorder not saved',
+        content: error,
+      });
+    }
   }
 });
 
 router.put('/:id', async (req, res) => {
   const {
-    recipe, startDate, status, actualHours,
+    woNumber, recipe, startDate, status, actualHours,
   } = req.body;
 
   const wo = {};
 
   wo.recipe = await Recipe.findOne({ name: recipe.name });
-
-  
-
-  //wo.recipe = recipe;
   wo.startDate = startDate;
   wo.status = status;
   wo.actualHours = actualHours;
+  wo.woNumber = woNumber;
+
   try {
-    const updatedWO = await WorkOrder.findByIdAndUpdate(req.params.id, wo);
+    const updatedWO = await WorkOrder.findOneAndUpdate({ _id: req.params.id }, wo);
     res.json({
       message: 'Updated work order',
       content: updatedWO,
