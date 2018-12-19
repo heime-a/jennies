089a21cc477f92c6f4fb4280fb969de3269989b0
Heime Ashkenazue <heime.a@gmail.com>
2018-12-19 18:25:23 -0500

    product inventory ui

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import { IngredientList } from './components/IngredientList';
 import PurchaseOrderList from "./components/PurchaseOrderList";
-import Inventory  from "./components/Inventory";
+import Inventory from "./components/Inventory";
+import ProductInventory from "./components/ProductInventory";
 import { RecipeList } from "./components/RecipeList";
 import  WorkOrderList from "./components/WorkOrderList";
 
@@ -20,7 +21,7 @@ import {
 
 class App extends Component {
   render() {
-    const menuItems = ['Ingredients','Purchasing','Inventory','Recipe','Manufacturing','Customer Orders']
+    const menuItems = ['Ingredients','Purchasing','Inventory','Recipe','Manufacturing','Product Inventory','Customer Orders']
     return <div className="App">
         <div className="App-header">
           <MyApp items={menuItems} />
@@ -31,27 +32,31 @@ class App extends Component {
 
 
 const MyApp = ({items}) => {
-  return (
-    <Router>
-      <div>   
+  return <Router>
+      <div>
         <Navbar color="light" expand="md">
           <NavbarBrand href="/">
-              <img src={require('./assets/jennies.jpg')} alt="Jennies Logo"/>
+            <img src={require("./assets/jennies.jpg")} alt="Jennies Logo" />
           </NavbarBrand>
-          <Collapse  navbar>
+          <Collapse navbar>
             <Nav className="ml-auto" navbar>
-                {items.map(item=>(<NavItem key={item}><Link className="nav-link" to={item}> {item}</Link></NavItem>))}
+              {items.map(item => <NavItem key={item}>
+                  <Link className="nav-link" to={item}>
+                    {" "}
+                    {item}
+                  </Link>
+                </NavItem>)}
             </Nav>
           </Collapse>
         </Navbar>
-        <Route path="/Ingredients" exact component={IngredientList}/>
+        <Route path="/Ingredients" exact component={IngredientList} />
         <Route path="/Purchasing" exact component={PurchaseOrderList} />
         <Route path="/Inventory" exact component={Inventory} />
         <Route path="/Recipe" exact component={RecipeList} />
         <Route path="/Manufacturing" exact component={WorkOrderList} />
+        <Route path="/Product Inventory" exact component={ProductInventory} />
       </div>
-</Router>
-  );
+    </Router>;
 };
 
 
