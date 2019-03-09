454cfc5b10269211f43dfc93453b24db16ad9e20
Heime Ash <heime@outlook.com>
2019-03-09 17:18:52 -0500

    excessive logging

--- client/src/common/isLoggedIn.js
+++ client/src/common/isLoggedIn.js
@@ -2,8 +2,7 @@ import apiUrl from './apiurl';
 
 function isLoggedIn() {
     const token = window.localStorage.getItem(`${apiUrl()}token`);
-    const reqStr = `${apiUrl()}auth/verify?token=${token}`;
-    console.log(reqStr);
+    //TODO: CHeck token to make sure its valid
     return token && token.length > 0;
 }
 
