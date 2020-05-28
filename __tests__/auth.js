f54912baf260ba892e4c7413811ab913f2d11856
Heime Ashkenazie <heime@outlook.com>
2020-05-28 15:29:45 -0400

    reorganized tests

--- __tests__/auth.js
+++ __tests__/auth.js
@@ -15,9 +15,8 @@ beforeAll(async (done) => {
 });
 afterAll(async (done) => { await hlp.destroyDB(); done(); });
 
-// This test fails because 1 !== 2
-it('can authorize a valid user', async (done) => {
 
+it('can authorize a valid user', async (done) => {
 
     const res = await request.post('/auth/signin').send({ email: 'user@example.com', password: 'abc123' });
     expect(res.body.success).toBeTruthy();
@@ -41,10 +40,8 @@ it('can deny an invalid user', async (done) => {
 
 it('can signup a user and sign him in', async () => {
     let res = await request.post('/auth/signup').send({ email: 'newuser@example.com', password: 'abc123' });
-
     expect(res.body.success).toBeTruthy()
     res = await request.post('/auth/signin').send({ email: 'newuser@example.com', password: 'abc123' });
-    // console.log(res.headers)
     expect(res.body.success).toBeTruthy()
 })
 it('cant sign up with a blank password', async () => {
@@ -57,25 +54,32 @@ it('cant sign up with a blank email', async () => {
     expect(res.body.success).toBeFalsy()
 })
 
-it('should fail accessing a restricted page', async () => {
-    const testSession = session(server);
-    //await testSession.post('/auth/signin').send({ email: 'user@example.com', password: 'abc123' })
-    const res = await testSession.get('/recipes');
-    expect(res.status).toBe(401)
-})
+describe('testing restricted routes', () => {
 
-it('can access a restricted page after logging in', async () => {
-    const testSession = session(server);
-    await testSession.post('/auth/signin').send({ email: 'user@example.com', password: 'abc123' })
-    const res = await testSession.get('/recipes');
-    expect(res.status).toBe(200);
-})
+    let authenticatedSession;
 
-it('cannot access a restricted page after logging out', async () => {
-    const testSession = session(server);
-    await testSession.post('/auth/signin').send({ email: 'user@example.com', password: 'abc123' })
-    await testSession.post('/auth/logout');
-    const res = await testSession.get('/recipes');
-    expect(res.status).toBe(401);
+    beforeEach(async () => {
+        const testSession = session(server);
+        const res = await testSession.post('/auth/signin').send({ email: 'user@example.com', password: 'abc123' })
+        expect(res.status).toBe(200)
+        authenticatedSession = testSession;
+    })
+
+    it('should fail accessing a restricted page', async () => {
+        const testSession = session(server);
+        const res = await testSession.get('/recipes');
+        expect(res.status).toBe(401)
+    })
+
+    it('can access a restricted page after logging in', async () => {
+        const res = await authenticatedSession.get('/recipes');
+        expect(res.status).toBe(200);
+    })
+
+    it('cant access a restricted page after logging out', async () => {
+        await authenticatedSession.post('/auth/logout');
+        const res = await authenticatedSession.get('/recipes');
+        expect(res.status).toBe(401);
+    })
 
 })
